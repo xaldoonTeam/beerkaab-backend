91b3c10d64da8ffd11ee84bf662a5db2fcbdc1a3
Abdiasis-Mustafe <tullirmustafe@gmail.com>
2024-12-28 12:54:42 +0300

    user requests

--- routes/users.ts
+++ routes/users.ts
@@ -3,6 +3,7 @@ import {
   login,
   updateUser,
   deleteUser,
+  getAllUsers,
 } from '../controllers/userController.ts';
 import express from 'express';
 import { decodeToken } from '../middleware/security/jwt.ts';
@@ -11,9 +12,9 @@ import { decodeToken } from '../middleware/security/jwt.ts';
 const router = express.Router();
 
 router.post('/create', createUser);
-
-router.get('/login', login);
+router.put('/login', login);
 router.put('/:id', updateUser);
+router.get("/All", getAllUsers)
 router.delete('/:id', deleteUser);
 
 
