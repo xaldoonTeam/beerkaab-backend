d775c26223dff5ebb5528ca1f178b586ae51a083
Abdiasis-Mustafe <tullirmustafe@gmail.com>
2025-01-15 23:19:01 +0300

    updated schema

--- routes/tools.ts
+++ routes/tools.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post('/create', createTool);
 router.get('/', getAllTools);
 router.put('/:id', updateTool);
-// router.delete('/:id', deleteTool);
-// router.get('/:id', getOneTool);
+router.delete('/:id', deleteTool);
+router.get('/:id', getOneTool);
 
 export default router;
